bbcfb51ca8bf2a57e444f822873ceda4d15b3f1a
ofoe-fiergbor <ofoefiergbor16@gmail.com>
2022-05-08 22:11:40 +0000

    regex implentation

--- src/main/java/PasswordPhilosophy.java
+++ src/main/java/PasswordPhilosophy.java
@@ -1,11 +1,13 @@
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PasswordPhilosophy {
 
 
     public static void main(String[] args) {
         List<String> input = List.of("1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc");
-        var countValidPassword = countValidPassword(input);
+        int countValidPassword = countValidPassword(input);
 
         System.err.println(countValidPassword);
 
@@ -13,25 +15,30 @@ public class PasswordPhilosophy {
 
     public static int countValidPassword(List<String> passwordPolicies) {
         int result = 0;
+        String regex = "^(\\d+)-(\\d+) (\\w): (.+)$";
+        Pattern pattern = Pattern.compile(regex);
 
         for (String passwordPolicy : passwordPolicies) {
-            String[] passwordPolicyArray = passwordPolicy.replace(":", "").split("[\s-]");
+            Matcher matcher = pattern.matcher(passwordPolicy);
 
-            String characterToTrack = passwordPolicyArray[2];
+            if (matcher.find()) {
+                int minCount = Integer.parseInt(matcher.group(1));
+                int maxCount = Integer.parseInt(matcher.group(2));
+                String characterToTrack = matcher.group(3);
+                String password = matcher.group(4);
 
-            int count = 0;
+                int count = 0;
 
-            String password = passwordPolicyArray[3];
-
-            for (int i = 0; i < password.length(); i++) {
-                if (characterToTrack.equals(String.valueOf(password.charAt(i)))) {
-                    count++;
+                for (int i = 0; i < password.length(); i++) {
+                    if (characterToTrack.equals(String.valueOf(password.charAt(i)))) {
+                        count++;
+                    }
+                }
+                if (count >= minCount && count <= maxCount) {
+                    result++;
                 }
             }
 
-            if (count >= Integer.parseInt(passwordPolicyArray[0]) && count <= Integer.parseInt(passwordPolicyArray[1])) {
-                result++;
-            }
         }
 
         return result;
